e7f3c1e73c8d67d6c31d122a819511510ab723ba
msozkilic <msozkilic@gmail.com>
2020-11-25 15:25:26 +0100

    Sepetten sil yapildi

--- src/test.java
+++ src/test.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class test {
+public class test {
+    public static void main(String[] args) {
+        System.out.println("1.islem");
+    }
+    public static void yeniMetod(){
+        System.out.println("yeni metod");
+        System.out.println("sepete ekle kisimlari eklendi");
+    }
+
+    private  static void SepettenSil(){
+        System.out.println("sepetten sil bolumu bitti");
+
+    }
 }
